39e16f0c9b42ec512b6bc7c24925ded48994e6e8
ericyangchen <yericchenyang@gmail.com>
2024-09-28 03:52:27 +0800

    fix: handle none image mask

--- scene/cameras.py
+++ scene/cameras.py
@@ -3,7 +3,7 @@
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 # All rights reserved.
 #
-# This software is free for non-commercial, research and evaluation use 
+# This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
 #
 # For inquiries contact  george.drettakis@inria.fr
@@ -46,7 +46,7 @@ class Camera(nn.Module):
         else:
             self.original_image *= torch.ones((1, self.image_height, self.image_width), device=self.data_device)
             self.gt_alpha_mask = None
-        
+
         self.zfar = 100.0
         self.znear = 0.01
 
@@ -70,4 +70,3 @@ class MiniCam:
         self.full_proj_transform = full_proj_transform
         view_inv = torch.inverse(self.world_view_transform)
         self.camera_center = view_inv[3][:3]
-
